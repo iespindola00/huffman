daac479c4be03dbf4f6d6e1092d7c646e0e605ab
iespindola <iespindola@educativa.com>
2022-05-23 19:06:18 -0300

    Argv y Argc

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "io.h"
 
-int main(){
+int main(int argc, char *argv[]){
+
+    if(argc != 3){
+        printf("Error en la cantidad de argumentos\n");
+        return 0;
+    }
+
+    if(strcmp(argv[1],"C") && strcmp(argv[1],"D")){
+        printf("Error en la accion a realizar\n");
+        return 0;
+    }
 
     printf("Listo\n");
 
