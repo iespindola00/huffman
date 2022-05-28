affac6ce1fc645ce0a677f4bbd973bfa0e70dc31
Lucas-Casariego <casariegolucas77@gmail.com>
2022-05-28 06:15:56 -0300

    codificación

--- main.c
+++ main.c
@@ -5,9 +5,47 @@
 #include "BTree/btree.h"
 #include "BTList/btlist.h"
 
+
+void codificacion_aux_arbol(BTree arbol, char** codificacion, char buf[], char nro){
+    
+    // si es nodo
+    if (arbol->caracter == -1){
+        // agrego un solo char
+        buf[strlen(buf)] = nro;
+        printf("buf: %s \t", buf);
+        codificacion_aux_arbol(arbol->left, codificacion, buf, '0');
+        codificacion_aux_arbol(arbol->right, codificacion, buf, '1');
+    }
+    // si es hoja
+    else{
+        // agrego un solo char
+        if(strlen(buf) + 1 >= 10000) { printf("error: el buf es demasiado pequeño"); }   
+        else {
+            buf[strlen(buf)] = nro;
+            buf[strlen(buf) + 1] = '\0';
+            //printf("buf: %s \t", buf);
+        }
+        codificacion[arbol->caracter] = malloc(sizeof(char) * strlen(buf) + 1);
+        strcpy(codificacion[arbol->caracter], buf);   // si ya terminó el camino hasta la hoja, le asigno su codificacion (buf) / atoi me parsea el buf a int
+        printf("codificación final: %s ", codificacion[arbol->caracter]);
+    }
+}
+
+char** codificacion_arbol(BTree arbol){
+    char** codificacion = malloc(sizeof(char*) * 256);
+    char buf[10000] = "";
+    codificacion_aux_arbol(arbol->left, codificacion, buf, '0');
+    codificacion_aux_arbol(arbol->right, codificacion, buf, '1');
+    printf("llegamos al final");
+
+    return codificacion;
+}
+
+
+/*
 char *comprimir_arbol( BTree arbol ){
 
-    char *buf = "";
+    char *buf;
     if(arbol->caracter == -1){
         strcat(buf, "0");
         strcat(buf, comprimir_arbol(arbol->left));
@@ -18,7 +56,7 @@ char *comprimir_arbol( BTree arbol ){
 
     return buf;
 }
-
+*/
 BTree arbol_huffman( BTList lista ){
 
     while ( lista->sig->sig != NULL){
@@ -76,8 +114,18 @@ int main(int argc, char *argv[]){
     }
 
     BTree arbol = arbol_huffman(listaNodos);
-    btree_imprimir(arbol);
-    //printf("Arbol final:%s\n", comprimir_arbol(arbol));
+
+    // creo arreglo para agregar los pares  char : codificación
+    char** codificacion;
+    codificacion = codificacion_arbol(arbol);
+
+    for (int i = 0; i < 256; i++){
+        printf("índice: %d codificación: %d", i, printf("codificación final: %s ", codificacion[i]));
+
+    }
+    
+    
+
 
     return 0;
 }
